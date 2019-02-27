1fdc2f3a76c3ae805f3afad623d6c23d56352dc5
skaws2003 <skaws2012@gmail.com>
2019-02-27 09:56:26 +0900

    commit to update

--- vocal_data.py
+++ vocal_data.py
@@ -10,11 +10,13 @@ class VocalData(torch.utils.data.Dataset):
     """
     A pytorch dataset class for vocal dataset.
     """
-    def __init__(self,root):
+    def __init__(self,root,length=20000):
         """
         :param root: directory to the dataset.
+        :param length: length of mfcc features to look at
         """
         self.root = root
+        self.length = length
         on_vocal = os.listdir(os.path.join(self.root,'on'))
         off_vocal = os.listdir(os.path.join(self.root,'off'))
         
@@ -31,7 +33,13 @@ class VocalData(torch.utils.data.Dataset):
     def __getitem__(self,index):
         filename,label = self.data[x]
         file = np.load(filename)
+        if file.shape[0] >= self.length:
+            file = file[:self.length]
+        else:
+            to_append = np.zeros((self.length - file.shape[0],file.shape[1]))
+            file = np.concatenate([file,to_append],axis=0)
         return file, label
+
 
 def get_vocaldata(root):
     """
--- vocal_unvocal.py
+++ vocal_unvocal.py
@@ -6,12 +6,21 @@ import torch.nn as nn
 import torch
 import torchvision
 
-class VocalUnvocal(torch.nn.modules):
-    def __init__(self):
-        raise NotImplementedError
+class VocalUnvocal(nn.modules):
+    def __init__(self,input_size,hidden_size,n_layers=1,dropout=0):
+        self.input_size = input_size
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.dropout = dropout
+        self.gru = nn.GRU(
+            input_size=self.input_size, hidden_size=self.hidden_size,
+            num_layers=self.n_layers, dropout = (0 if n_layers==1 else self.dropout),
+            bidirectional = True
+        )
 
-    def __len__(self):
-        raise NotImplementedError
-
-    def __getitem__(self,x):
-        raise NotImplementedError
+    def forward(self, mfcc_features,lengths,hidden=None):
+        packed = nn.utils.rnn.pack_padded_sequence(mfcc_features,lengths)
+        outputs, hidden = self.gru(packed,hidden)
+        outputs,_ = nn.utils.rnn.pad_packed_sequence(outputs)
+        outputs = outputs[:,:,self.hidden_size] + outputs[:,:,self.hidden_size:]
+        return outputs, hidden
